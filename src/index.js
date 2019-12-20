0e5f9574359b6efcbae772e5e404797f8052ea53
Karthik Reddy <karthikreddy.chinasani1995@gmail.com>
2019-12-20 12:57:35 +0530

    Added code for swagger api

--- src/index.js
+++ src/index.js
@@ -1,42 +1,54 @@
-import http from 'http';
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import bodyParser from 'body-parser';
-import initializeDb from './db';
-import middleware from './middleware';
-import api from './api';
-import config from './config.json';
-import dotenv from 'dotenv';
+import http from "http";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import initializeDb from "./db";
+import middleware from "./middleware";
+import api from "./api";
+import config from "./config.json";
+import dotenv from "dotenv";
+import expressOasGenerator from "express-oas-generator";
+import _ from 'loadsh';
+
 dotenv.config();
 
 let app = express();
+expressOasGenerator.init(app, function(spec) {
+    _.set(spec, 'info.title', 'BC Hack');
+    _.set(spec, 'paths[\'/path\'].get.parameters[0].example', 2);
+    return spec;
+});
+
 app.server = http.createServer(app);
 
 // logger
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // 3rd party middleware
-app.use(cors({
-	exposedHeaders: config.corsHeaders
-}));
+app.use(
+  cors({
+    exposedHeaders: config.corsHeaders
+  })
+);
 
-app.use(bodyParser.json({
-	limit : config.bodyLimit
-}));
+app.use(
+  bodyParser.json({
+    limit: config.bodyLimit
+  })
+);
 
 // connect to db
-initializeDb( db => {
+initializeDb(db => {
+  // internal middleware
+  app.use(middleware({ config, db }));
 
-	// internal middleware
-	app.use(middleware({ config, db }));
+  // api router
+  app.use("/api", api({ config, db }));
 
-	// api router
-	app.use('/api', api({ config, db }));
-
-	app.server.listen(process.env.PORT || config.port, () => {
-		console.log(`Started on port ${app.server.address().port}`);
-	});
+  app.server.listen(process.env.PORT || config.port, () => {
+    console.log(`Started on port ${app.server.address().port}`);
+  });
 });
 
 export default app;
